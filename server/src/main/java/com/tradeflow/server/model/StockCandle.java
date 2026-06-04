444173c20669f65a70d4520596a8be254e5943fb
rarebuffalo <info.krishnasingh.codes@gmail.com>
2026-06-04 10:50:20 +0530

    removed lombok from model classes and wrote getters/setters manually

--- server/src/main/java/com/tradeflow/server/model/StockCandle.java
+++ server/src/main/java/com/tradeflow/server/model/StockCandle.java
@@ -1,8 +1,5 @@
 package com.tradeflow.server.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
@@ -10,9 +7,6 @@ import org.springframework.data.cassandra.core.mapping.Table;
 import java.math.BigDecimal;
 
 @Table("stock_candles")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class StockCandle {
 
     @PrimaryKey
@@ -32,4 +26,64 @@ public class StockCandle {
 
     @Column("volume")
     private Long volume;
+
+    public StockCandle() {
+    }
+
+    public StockCandle(StockCandleKey key, BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal close, Long volume) {
+        this.key = key;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+    }
+
+    public StockCandleKey getKey() {
+        return key;
+    }
+
+    public void setKey(StockCandleKey key) {
+        this.key = key;
+    }
+
+    public BigDecimal getOpen() {
+        return open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getClose() {
+        return close;
+    }
+
+    public void setClose(BigDecimal close) {
+        this.close = close;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
 }
--- server/src/main/java/com/tradeflow/server/model/StockCandleKey.java
+++ server/src/main/java/com/tradeflow/server/model/StockCandleKey.java
@@ -1,8 +1,5 @@
 package com.tradeflow.server.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
@@ -11,11 +8,9 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import java.io.Serializable;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @PrimaryKeyClass
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class StockCandleKey implements Serializable {
 
     @PrimaryKeyColumn(name = "symbol", type = PrimaryKeyType.PARTITIONED, ordinal = 0)
@@ -26,4 +21,52 @@ public class StockCandleKey implements Serializable {
 
     @PrimaryKeyColumn(name = "datetime", type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING, ordinal = 2)
     private Instant datetime;
+
+    public StockCandleKey() {
+    }
+
+    public StockCandleKey(String symbol, LocalDate tradeDate, Instant datetime) {
+        this.symbol = symbol;
+        this.tradeDate = tradeDate;
+        this.datetime = datetime;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public LocalDate getTradeDate() {
+        return tradeDate;
+    }
+
+    public void setTradeDate(LocalDate tradeDate) {
+        this.tradeDate = tradeDate;
+    }
+
+    public Instant getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Instant datetime) {
+        this.datetime = datetime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StockCandleKey that = (StockCandleKey) o;
+        return Objects.equals(symbol, that.symbol) &&
+                Objects.equals(tradeDate, that.tradeDate) &&
+                Objects.equals(datetime, that.datetime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbol, tradeDate, datetime);
+    }
 }
