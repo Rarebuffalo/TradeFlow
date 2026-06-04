2115dc72d4050adc0032a4b85437a3919cdbe09b
rarebuffalo <info.krishnasingh.codes@gmail.com>
2026-06-04 10:50:24 +0530

    removed lombok annotations from api response dtos

--- server/src/main/java/com/tradeflow/server/dto/AggregatedCandle.java
+++ server/src/main/java/com/tradeflow/server/dto/AggregatedCandle.java
@@ -1,17 +1,8 @@
 package com.tradeflow.server.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.Instant;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class AggregatedCandle {
     private Instant datetime;
     private BigDecimal open;
@@ -19,4 +10,64 @@ public class AggregatedCandle {
     private BigDecimal low;
     private BigDecimal close;
     private Long volume;
+
+    public AggregatedCandle() {
+    }
+
+    public AggregatedCandle(Instant datetime, BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal close, Long volume) {
+        this.datetime = datetime;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+    }
+
+    public Instant getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Instant datetime) {
+        this.datetime = datetime;
+    }
+
+    public BigDecimal getOpen() {
+        return open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getClose() {
+        return close;
+    }
+
+    public void setClose(BigDecimal close) {
+        this.close = close;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
 }
--- server/src/main/java/com/tradeflow/server/dto/CandleResponse.java
+++ server/src/main/java/com/tradeflow/server/dto/CandleResponse.java
@@ -1,19 +1,52 @@
 package com.tradeflow.server.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class CandleResponse {
     private String symbol;
     private String timeframe;
     private List<AggregatedCandle> candles;
     private int count;
+
+    public CandleResponse() {
+    }
+
+    public CandleResponse(String symbol, String timeframe, List<AggregatedCandle> candles, int count) {
+        this.symbol = symbol;
+        this.timeframe = timeframe;
+        this.candles = candles;
+        this.count = count;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getTimeframe() {
+        return timeframe;
+    }
+
+    public void setTimeframe(String timeframe) {
+        this.timeframe = timeframe;
+    }
+
+    public List<AggregatedCandle> getCandles() {
+        return candles;
+    }
+
+    public void setCandles(List<AggregatedCandle> candles) {
+        this.candles = candles;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
